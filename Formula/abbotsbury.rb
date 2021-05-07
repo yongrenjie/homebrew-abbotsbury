f5723aad59cb0d2aa6f47de5ca34be926d47779e
Jonathan Yong <yongrenjie@gmail.com>
2021-05-07 19:30:58 +0100

    Fix indentation

--- Formula/abbotsbury.rb
+++ Formula/abbotsbury.rb
@@ -3,9 +3,8 @@ class Abbotsbury < Formula
   homepage "https://github.com/yongrenjie/abbotsbury"
   url "https://github.com/yongrenjie/abbotsbury.git", tag: "v0.1.0.9", revision: "5d8a06e95ea9b45e4f02ee4f62e80aa54ce196d8"
 
-bottle do
+  bottle do
     root_url "https://github.com/yongrenjie/abbotsbury/releases/download/v0.1.0.9"
-    
     sha256 cellar: :any, catalina: "ae3c48d2171d82e8622bec3b7f19bc4534d067c30c96ac63f9d400d2ca9d1cb4"
   end
 
