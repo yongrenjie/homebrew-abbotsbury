45e8382fbd0a50178673da27d24a927889e680f5
Jonathan Yong <yongrenjie@gmail.com>
2021-05-07 19:29:02 +0100

    Update abbotsbury to v0.1.0.9

--- Formula/abbotsbury.rb
+++ Formula/abbotsbury.rb
@@ -2,6 +2,13 @@ class Abbotsbury < Formula
   desc "Minimalistic command-line reference manager"
   homepage "https://github.com/yongrenjie/abbotsbury"
   url "https://github.com/yongrenjie/abbotsbury.git", tag: "v0.1.0.9", revision: "5d8a06e95ea9b45e4f02ee4f62e80aa54ce196d8"
+
+bottle do
+    root_url "https://github.com/yongrenjie/abbotsbury/releases/download/v0.1.0.9"
+    
+    sha256 cellar: :any, catalina: "ae3c48d2171d82e8622bec3b7f19bc4534d067c30c96ac63f9d400d2ca9d1cb4"
+  end
+
   license "MIT"
 
   # much of this is taken from pandoc's formula.
