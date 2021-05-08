c184dcbc2ad7c17bf526cee854c4534ca77dddb2
Jonathan Yong <yongrenjie@gmail.com>
2021-05-08 17:20:51 +0100

    Fix license

--- Formula/abbotsbury.rb
+++ Formula/abbotsbury.rb
@@ -1,6 +1,7 @@
 class Abbotsbury < Formula
   desc "Minimalistic command-line reference manager"
   homepage "https://github.com/yongrenjie/abbotsbury"
+  license "MIT"
   url "https://github.com/yongrenjie/abbotsbury.git", tag: "v0.1.0.10", revision: "00080be8312e1a4a72b85ad3e46d8ad8dfe5b2bf"
 
   bottle do
@@ -8,12 +9,6 @@ class Abbotsbury < Formula
     sha256 cellar: :any, catalina: "a36b1dff543e3a9b007637c44d334a5a063b8f8bafd62a9ce4c822096451de39"
   end
 
-
-  
-
-  license "MIT"
-
-  # much of this is taken from pandoc's formula.
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
 
