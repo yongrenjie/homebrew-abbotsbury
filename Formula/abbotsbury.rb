4d1b95123b144e2d5b912eb42813b2ecdacc1f1e
Jonathan Yong <yongrenjie@gmail.com>
2021-05-09 03:51:50 +0100

    Update abbotsbury to v0.1.0.11

--- Formula/abbotsbury.rb
+++ Formula/abbotsbury.rb
@@ -4,6 +4,13 @@ class Abbotsbury < Formula
   license "MIT"
   url "https://github.com/yongrenjie/abbotsbury.git", tag: "v0.1.0.11", revision: "e7586138130e1565efbcb23a8d50a0e46d9ab0fb"
 
+bottle do
+  root_url "https://github.com/yongrenjie/abbotsbury/releases/download/v0.1.0.11"
+  
+  sha256 cellar: :any, catalina: "8e17b5867c9e3a4d42f781aa611ca1c104dc4573819827de5f34f1fa2716257d"
+end
+
+
   
 
   depends_on "cabal-install" => :build
