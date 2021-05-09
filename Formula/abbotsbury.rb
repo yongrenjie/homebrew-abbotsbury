f112fa6a0c8cbd9cb7fd95a2247478c618327709
Jonathan Yong <yongrenjie@gmail.com>
2021-05-09 03:33:22 +0100

    Update abbotsbury to v0.1.0.11

--- Formula/abbotsbury.rb
+++ Formula/abbotsbury.rb
@@ -2,12 +2,9 @@ class Abbotsbury < Formula
   desc "Minimalistic command-line reference manager"
   homepage "https://github.com/yongrenjie/abbotsbury"
   license "MIT"
-  url "https://github.com/yongrenjie/abbotsbury.git", tag: "v0.1.0.10", revision: "00080be8312e1a4a72b85ad3e46d8ad8dfe5b2bf"
+  url "https://github.com/yongrenjie/abbotsbury.git", tag: "v0.1.0.11", revision: "e7586138130e1565efbcb23a8d50a0e46d9ab0fb"
 
-  bottle do
-    root_url "https://github.com/yongrenjie/abbotsbury/releases/download/v0.1.0.10"
-    sha256 cellar: :any, catalina: "a36b1dff543e3a9b007637c44d334a5a063b8f8bafd62a9ce4c822096451de39"
-  end
+  
 
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
