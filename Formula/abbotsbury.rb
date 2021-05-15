62c7c0ba99402c6a844d674a67f8d3e4817de64c
Jonathan Yong <yongrenjie@gmail.com>
2021-05-15 19:31:57 +0100

    Update abbotsbury to v0.2.0.0

--- Formula/abbotsbury.rb
+++ Formula/abbotsbury.rb
@@ -4,6 +4,13 @@ class Abbotsbury < Formula
   license "MIT"
   url "https://github.com/yongrenjie/abbotsbury.git", tag: "v0.2.0.0", revision: "71507b7d1bc1c1f8d38fca0d36e03c1f91928413"
 
+bottle do
+  root_url "https://github.com/yongrenjie/abbotsbury/releases/download/v0.2.0.0"
+  
+  sha256 cellar: :any, catalina: "1677edd597a80c450fe9425556acdbe3a0aa1724c984d2a1ee104e7c57dad14d"
+end
+
+
 
 
 
