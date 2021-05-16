415066873ba1d0db6da63e806674f98571beed4b
Jonathan Yong <yongrenjie@gmail.com>
2021-05-16 03:06:08 +0100

    fix whitespace

--- Formula/abbotsbury.rb
+++ Formula/abbotsbury.rb
@@ -4,29 +4,10 @@ class Abbotsbury < Formula
   license "MIT"
   url "https://github.com/yongrenjie/abbotsbury.git", tag: "v0.2.1.0", revision: "eabd177a7f0e17236c7cd9add61bd74839704cca"
 
-bottle do
-  root_url "https://github.com/yongrenjie/abbotsbury/releases/download/v0.2.1.0"
-  
-  sha256 cellar: :any, catalina: "5da16141ad5864d48c6ad3ad66fc8f2246972e1e63720b026e07bfab2415163f"
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+  bottle do
+    root_url "https://github.com/yongrenjie/abbotsbury/releases/download/v0.2.1.0"
+    sha256 cellar: :any, catalina: "5da16141ad5864d48c6ad3ad66fc8f2246972e1e63720b026e07bfab2415163f"
+  end
 
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
